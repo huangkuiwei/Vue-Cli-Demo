0ad93d983120603f5d3adfbb105d55a837b72ff3
huangkui <1017969390@qq.com>
2018-12-01 21:45:24 +0800

    项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');     //引入 node.js -> path包
+const {VueLoaderPlugin} = require('vue-loader');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';      //定义mode环境
+
+module.exports = {
+  mode: devMode ? 'development' : 'production',
+  entry: {
+    app: './src/main.js'      //入口
+  },
+  output: {
+    filename: devMode ? "[name].js" : '[name].js?[chunkhash8]'      //出口
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@views': path.resolve(__dirname, 'src/views')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']      //把ES6 ES7等语法转换为ES5
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.(.jpg | .png | .gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            outputPath: devMode ? '[name].[ext]' : '[name].[ext]?[hash8]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new CleanWebpackPlugin('dist'),
+    new HtmlWebpackPlugin({
+      inject: false,
+      title: 'Hello Vue.js',
+      template: require('html-webpack-template'),
+      bodyHtmlSnippet: '<div id="app"></div>',
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1'
+        }
+      ],
+      minify: {
+        collapseWhitespace: true,                 //删除空格，压缩html
+        removeScriptTypeAttributes: true,         //清除所有script标签中的type="text/javascript"属性
+        removeStyleLinkTypeAttributes: true       //清楚所有Link标签上的type属性
+      }
+    })
+  ],
+  devServer: {
+    port: 8888
+  }
+}
+;
