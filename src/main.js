30f7b4f4c47bfea224b86b9966371cd29cc4dde7
huangkui <1017969390@qq.com>
2019-02-14 17:14:12 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import './style.css'
 import Vue from 'vue'
 import App from './app.vue'
 import Axios from 'axios'
+import Vuex from 'vuex'
 import router from './router'
 import Layer from './plugins/layer'
 import Toast from './plugins/toast'
 import ModalDialog from './plugins/components/modal-dialog.vue'
 
 Vue.use(Axios);
+Vue.use(Vuex);
 Vue.use(Layer);
 Vue.use(Toast);
 
